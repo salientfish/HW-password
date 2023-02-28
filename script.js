c9188a60965d1bad3b102f7759fe1cae9657d007
salientfish <120330497+salientfish@users.noreply.github.com>
2023-02-27 18:17:02 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,14 @@
-const pwordfield = document.querySelector('#password');
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-var upper = ['A', 'B', 'C'];
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-var lower = ['a', 'b', 'c'];
+  passwordText.value = password;
 
-var number = ['1', '2', '3', '4'];
-
-var specialChar = ['!','@','#','$','%','^','&','*','(',')'];
-var splitChar = specialChar[0].split('');
-
-console.log(specialChar, ':', splitChar);
-
-var upperSelect = confirm('upper?');
-var lowerSelect = confirm('lower?');
-var numberSelect = confirm('number?');
-var specialSelect = confirm('special Characters?');
-var pwdLength = null;
-var getLength = function() {
-    pwdLength = prompt('pwd length');
-    if(pwdLength < 8 || pwdLength > 128){
-        alert('password length wrong')
-        getLength();
-    }    
-};
-
-var dataSet = [];
-
-
-if(upperSelect){
-    dataSet = dataSet.concat(upper);
 }
-if(lowerSelect){
-    dataSet = dataSet.concat(lower);
-}
-if(numberSelect){
-    dataSet = dataSet.concat(number);
-}
-if(specialSelect){
-    dataSet = dataSet.concat(specialChar);
-}
-console.log(dataSet);
 
-getLength();
-
-var pwdString = '';
-for (let i = 0; i < pwdLength; i++){
-    pwdString = pwdString.concat(dataSet[Math.floor(Math.random() * dataSet.length)]);
-}
-console.log(pwdString);
-
-pwordfield.value = pwdString;
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
